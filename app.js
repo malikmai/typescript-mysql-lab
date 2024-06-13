aa31ae4a9f19f01b8e131df77b3543b6927fcc91
R.Jones <RayshawnJones1991@gmail.com>
2024-06-13 15:29:16 -0400

    Rayshawn file for mysql-typescript lesson

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var mysql = require("mysql2");
-var prompt = require('prompt-sync')();
+var prompt = require("prompt-sync")();
 // Set up the MySQL connection
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'CRM',
+    host: "localhost",
+    user: "root",
+    database: "CRM",
 });
 // Function to view all customers
 var viewAllCustomers = function () {
-    connection.query('SELECT * FROM customers', function (err, results, fields) {
-        console.log('Query results:', results);
+    connection.query("SELECT * FROM customers", function (err, results, fields) {
+        console.log("Query results:", results);
         connection.end();
     });
 };
+// Function to Create new Customer
+var createCustomer = function () {
+    var firstName = prompt("Enter first name: ");
+    var lastName = prompt("Enter last name: ");
+    var age = prompt("Enter age: ");
+    var sql = 'INSERT INTO customers (first_name, last_name, age) VALUES (?,?,?)';
+    var values = [firstName, lastName, age];
+    connection.query(sql, values, function (err, results, fields) {
+        if (err) {
+            console.error('Error,inserting customer:', err);
+            return;
+        }
+        console.log('Customer Created Successfully', results);
+        connection.end();
+    });
+};
+//Function to Edit
+//Function to Update
+//Function to Delete
+//     connection.query('INSERT INTO customers (first_name, last_name, age) VALUES (?,?,?)', [firstName, lastName, age], (err, results, fields) => {
+//         console.log('Query results:', results);
+//         connection.end();
+//     });
+// };
 // Main function to handle user input
 var main = function () {
-    console.log('Welcome to your CRM');
-    console.log('1. View all customers');
-    console.log('2. Exit');
-    var choice = prompt('Please enter your choice: ');
+    console.log("Welcome to your CRM");
+    console.log("1. View all customers");
+    console.log("2. Create Customer");
+    console.log("3. Edit Customer");
+    console.log("4. Exit");
+    var choice = prompt("Please enter your choice: ");
     switch (choice) {
-        case '1':
+        case "1":
             viewAllCustomers();
             break;
-        case '2':
-            console.log('Exiting...');
-            connection.end();
+        case "2":
+            console.log('Customer Created');
             break;
+        case "3":
+            console.log('Customer Edited');
+        case "4":
+            console.log('Exiting...');
         default:
-            console.log('Invalid choice. Exiting...');
+            console.log("Invalid choice. Exiting...");
             connection.end();
             break;
     }
 };
 // Connect to the MySQL database
 connection.connect(function () {
-    console.log('Connected to the database');
+    console.log("Connected to the database");
     main();
 });
--- app.ts
+++ app.ts
@@ -1,46 +1,81 @@
 import * as mysql from "mysql2";
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 // Set up the MySQL connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'CRM',
+  host: "localhost",
+  user: "root",
+  database: "CRM",
 });
 
 // Function to view all customers
 const viewAllCustomers = () => {
-    connection.query('SELECT * FROM customers', (err, results, fields) => {
-        console.log('Query results:', results);
-        connection.end();
-    });
+  connection.query("SELECT * FROM customers", (err, results, fields) => {
+    console.log("Query results:", results);
+    connection.end();
+  });
 };
+
+// Function to Create new Customer
+
+const createCustomer = () => {
+  const firstName = prompt("Enter first name: ");
+  const lastName = prompt("Enter last name: ");
+  const age = prompt("Enter age: ");
+  const sql = 'INSERT INTO customers (first_name, last_name, age) VALUES (?,?,?)';
+  const values = [firstName, lastName, age];
+  connection.query(sql, values, (err, results, fields) => {
+    if (err) {
+        console.error('Error,inserting customer:', err);
+        return;
+    }
+    console.log('Customer Created Successfully', results);
+    connection.end();
+});
+}
+
+//Function to Edit
+
+//Function to Update
+
+//Function to Delete
+
+//     connection.query('INSERT INTO customers (first_name, last_name, age) VALUES (?,?,?)', [firstName, lastName, age], (err, results, fields) => {
+//         console.log('Query results:', results);
+//         connection.end();
+//     });
+// };
 
 // Main function to handle user input
 const main = () => {
-    console.log('Welcome to your CRM');
-    console.log('1. View all customers');
-    console.log('2. Exit');
+  console.log("Welcome to your CRM");
+  console.log("1. View all customers");
+  console.log("2. Create Customer");
+  console.log("3. Edit Customer");
+  console.log("4. Exit");
 
-    const choice = prompt('Please enter your choice: ');
+  const choice = prompt("Please enter your choice: ");
 
-    switch (choice) {
-        case '1':
-            viewAllCustomers();
-            break;
-        case '2':
-            console.log('Exiting...');
-            connection.end();
-            break;
-        default:
-            console.log('Invalid choice. Exiting...');
-            connection.end();
-            break;
-    }
+  switch (choice) {
+    case "1":
+      viewAllCustomers();
+      break;
+    case "2":
+      console.log('Customer Created')
+      break;
+    case "3":
+        console.log('Customer Edited')
+        case "4":
+            console.log('Exiting...')
+    default:
+      console.log("Invalid choice. Exiting...");
+      connection.end();
+      break;
+  }
 };
 
 // Connect to the MySQL database
 connection.connect(() => {
-    console.log('Connected to the database');
-    main();
+  console.log("Connected to the database");
+  main();
 });
