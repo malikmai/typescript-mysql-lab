a11c75d5ff24cf888976585b8244185569e41e0f
chris <jimenezchristopher5@gmail.com>
2024-06-13 13:38:05 -0500

    edited database name

--- app.ts
+++ app.ts
@@ -4,12 +4,13 @@ import * as mysql from "mysql2";
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'CRM',
+    password: 'cratose@41795',
+    database: 'Customers',
 });
 
-connection.connect(() => {
-    console.log(`Conencted to the MySql server`);
-});
+// connection.connect(() => {
+//     console.log(`Conencted to the MySql server`);
+// });
 
 connection.query('SELECT * FROM customers', (err, results) => {
     console.log(results);
